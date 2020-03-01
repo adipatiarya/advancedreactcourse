f3cbfac8a6e8aa4029eb066c44792202cf85815e
Adipati Arya <arya@Adipatis-MacBook-Pro.local>
2020-03-02 06:32:40 +0700

    Rendering a Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React , {Component} from 'react';
+import { Route, Link } from 'react-router-dom';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-export default () => {
-    return (
-        <div>
-           <Route path="/post" component={CommentBox}/>
-           <Route path="/" exact component={CommentList}/>
-        </div>
-    )
+export default class App extends Component  {
+
+    renderButton = () => {
+        return (
+            <Link to="/login">Login</Link>
+        )
+    }
+    renderHeader = () => {
+        return (
+            <nav>
+                <Link to="/">Home</Link> {" "}
+                <Link to="/post">Post</Link> {" "}
+                {this.renderButton()}
+            </nav>
+        )
+    }
+    render() {
+        return (
+            <div>
+                {this.renderHeader()}
+               <Route path="/post" component={CommentBox}/>
+               <Route path="/" exact component={CommentList}/>
+            </div>
+        )
+    }
 };
