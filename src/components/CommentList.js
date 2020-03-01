561172c6c5c5b66708abdf61cf66e62be12c503a
Adipati Arya <arya@Adipatis-MacBook-Pro.local>
2020-03-02 06:00:47 +0700

    Adding Routes

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -9,9 +9,12 @@ class CommentList extends Component {
 
     render() {
         return (
-            <ul>
-                {this.renderComments()}
-            </ul>
+            <div>
+                <h4>Comment List</h4>
+                <ul>
+                    {this.renderComments()}
+                </ul>
+            </div>
         )
     }
 }
