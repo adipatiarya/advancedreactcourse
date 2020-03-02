4631b800e3b7a8272ae9b11fc99a28152041e998
Adipati Arya <arya@Adipatis-MacBook-Pro.local>
2020-03-02 08:04:47 +0700

    Create High Order Component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -2,25 +2,15 @@ import React , { Component } from 'react';
 import {connect} from 'react-redux';
 import * as actions from 'actions';
 
+import requireAuth from 'components/requireAuth';
+
+
 class CommentBox extends Component {
 
     state = {
         comment:''
     }
-    componentDidMount () {
-
-        this.shouldNavigateAway();
-    }
-    componentDidUpdate () {
-        
-        this.shouldNavigateAway();
-
-    }
-    shouldNavigateAway () {
-        if(!this.props.auth) {
-            this.props.history.push('/');
-        }
-    }
+    
 
     handleChange = (event) => {
         this.setState({comment:event.target.value});
@@ -45,9 +35,5 @@ class CommentBox extends Component {
         )
     }
 }
-function mapStateToProps(state) {
-    return {
-        auth:state.auth
-    }
-}
-export default connect(mapStateToProps, actions)(CommentBox);
+
+export default connect(null, actions)(requireAuth(CommentBox));
